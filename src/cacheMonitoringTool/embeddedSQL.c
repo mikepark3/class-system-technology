dff332bd272ecaa773198cf3266ffc0247d8a396
Park Seong U <qkrtjddn938@naver.com>
2015-04-06 01:31:34 +0900

    second commit

--- src/cacheMonitoringTool/embeddedSQL.c
+++ src/cacheMonitoringTool/embeddedSQL.c
@@ -1,24 +1,24 @@
-#include <mysql.h>
+#include <mysql/mysql.h>
 #include <string.h>
 #include <stdio.h>
-#define DB_HOST "127.0.0.1"
+#define DB_HOST "192.168.0.5"
 #define DB_USER "root"
-#define DB_PASS "gkwlak"
-#define DB_NAME "test"
+#define DB_PASS "1234"
+#define DB_NAME "my_report"
 #define CHOP(x) x[strlen(x) - 1] = ' '
 
 int main(void)
 {
-	MYSQL       *connection=NULL, conn;
+	MYSQL       *connection, conn;
 	MYSQL_RES   *sql_result;
 	MYSQL_ROW   sql_row;
 	int       query_stat;
-	char name[12];
-	char address[80];
-	char tel[12];
-	char query[255];
+
+	//Initialize
 	mysql_init(&conn);
 
+	//Connection
+	connection = NULL;
 	connection = mysql_real_connect(&conn, DB_HOST, DB_USER, DB_PASS, DB_NAME, 3306, (char *)NULL, 0);
 	if(connection == NULL)
 	{
@@ -26,35 +26,32 @@ int main(void)
 		return 1;
 	}
 
-	query_stat = mysql_query(connection, "select * from address");
+	//Select Query
+	query_stat = mysql_query(connection, "select * from mydata");
 	if(query_stat != 0)
 	{
 		fprintf(stderr, "Mysql query error : %s", mysql_error(&conn));
 		return 1;
 	}
-	sql_result = mysql_store_result(connection);
-	printf("%+11s %-30s %-10s", "이름", "주소", "전화번호");
 
+	//Print
+	printf("%+11s %-5s", "time", "temperature");
+	sql_result = mysql_store_result(connection);
 	while((sql_row = mysql_fetch_row(sql_result)) != NULL)
 	{
-		printf("%+11s %-30s %-10s", sql_row[0], sql_row[1], sql_row[2]);
+		printf("%+11s %-5s", sql_row[0], sql_row[1]);
 	}
+
 	mysql_free_result(sql_result);
-	printf("이름 :");
-	fgets(name, 12, stdin);
-	CHOP(name);
-	printf("주소 :");
-	fgets(address, 80, stdin);
-	CHOP(address);
-	printf("전화 :");
-	fgets(tel, 12, stdin);
-	CHOP(tel);
-	sprintf(query, "insert into address values ""('%s', '%s', '%s')", name, address, tel);
+
+	//Insert Query
+	sprintf(query, "insert into mydata values ""('%s', '%f')", "2015-12-31", 13.1);
 	query_stat = mysql_query(connection, query);
 	if(query_stat != 0)
 	{
 		fprintf(stderr, "Mysql query error : %s", mysql_error(&conn));
 		return 1;
 	}
+
 	mysql_close(connection);
 }
